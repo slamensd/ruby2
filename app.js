a18d24fe9225f93bc207ff4278c4ce5a7bd279ba
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-23 19:39:02 +0000

    ff

--- app.js
+++ app.js
@@ -34,101 +34,86 @@ window.onload = async () => {
         }
     ];
 
-    let contract = new ethers.Contract(contractAddress, erc1155Abi, provider);
+    // ...previous code
 
-    document.getElementById("connectBtn").addEventListener("click", async () => {
-        try {
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            userAddress = accounts[0];
-            document.getElementById("walletAddress").innerText = `Connected: ${userAddress}`;
-            console.log("Wallet connected:", userAddress);
+let camera, scene, renderer, flashlight, hiddenMessageMaterial;
 
-            // Get balance of Token 6
-            let tokenId = 6;
-            let balance = await contract.balanceOf(userAddress, tokenId);
-            document.getElementById("tokenBalance").innerText = `Token 6 Balance: ${balance.toString()}`;
-        } catch (error) {
-            console.error("Error connecting to wallet:", error);
-        }
-    });
+function init() {
+    const container = document.getElementById('hiddenMessageContainer');
+    const width = container.clientWidth;
+    const height = container.clientHeight;
 
-    document.getElementById("verifyBtn").addEventListener("click", async () => {
-        try {
-            let tokenId = 6;
-            let balance = await contract.balanceOf(userAddress, tokenId);
-            console.log("Token balance:", balance.toString());
+    scene = new THREE.Scene();
 
-            if (balance > 0) {
-                document.getElementById("verificationMessage").innerText = "Token Verified!";
-                document.getElementById("hiddenMessage").innerText = "We start at zero, book one unfurled, Page one's second letter \"U\", into our world. To page three we turn, the fifth letter to peek, On this journey of ours, the clues we seek. Fast forward to eight, a letter to meet, Put them together, the word is complete. A three-letter puzzle, for your mind's keep, Seek, find, and unravel, in this mystery deep.";
-            } else {
-                document.getElementById("verificationMessage").innerText = "Error: Token not found in this wallet!";
-            }
-        } catch (error) {
-            console.error("Error during token verification:", error);
-        }
-    });
+    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.z = 5;
 
-    // Flashlight Effect using Three.js
-    let flashlight;
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(width, height);
+    container.appendChild(renderer.domElement);
 
-    function initFlashlight() {
-        const container = document.getElementById('hiddenMessageContainer');
-        const width = container.clientWidth;
-        const height = container.clientHeight;
+    flashlight = new THREE.SpotLight(0xffffff, 2);
+    flashlight.position.set(0, 0, 5);
+    flashlight.angle = Math.PI / 4;
+    flashlight.penumbra = 0.2;
+    flashlight.decay = 2;
+    flashlight.distance = 50;
+    scene.add(flashlight);
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-        camera.position.z = 5;
+    const geometry = new THREE.PlaneGeometry(5, 2);
 
-        const renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setSize(width, height);
-        container.appendChild(renderer.domElement);
+    hiddenMessageMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0 });
 
-        flashlight = new THREE.SpotLight(0xffffff, 2);
-        flashlight.position.set(0, 0, 5);
-        flashlight.angle = Math.PI / 4;
-        flashlight.penumbra        = 0.2;
-        flashlight.decay = 2;
-        flashlight.distance = 50;
-        scene.add(flashlight);
+    const hiddenMessage = new THREE.Mesh(geometry, hiddenMessageMaterial);
+    scene.add(hiddenMessage);
 
-        const geometry = new THREE.PlaneGeometry(5, 2);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0 });
+    animate();
+}
 
-        const hiddenMessage = new THREE.Mesh(geometry, material);
-        scene.add(hiddenMessage);
+function animate() {
+    requestAnimationFrame(animate);
 
-        function animate() {
-            requestAnimationFrame(animate);
+    flashlight.position.copy(camera.position);
 
-            flashlight.position.copy(camera.position);
+    renderer.render(scene, camera);
+}
 
-            renderer.render(scene, camera);
-        }
+document.getElementById('hiddenMessageContainer').addEventListener('mousemove', (event) => {
+    const container = document.getElementById('hiddenMessageContainer');
+    const rect = container.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
-        animate();
+    const normalizedX = (x / rect.width) * 2 - 1;
+    const normalizedY = -(y / rect.height) * 2 + 1;
+
+    const mouseVector = new THREE.Vector3(normalizedX, normalizedY, 0.5);
+    mouseVector.unproject(camera);
+
+    const dir = mouseVector.sub(camera.position).normalize();
+    const distance = -camera.position.z / dir.z;
+    const pos = camera.position.clone().add(dir.multiplyScalar(distance));
+
+    flashlight.target.position.copy(pos);
+
+    // Calculate the distance from the mouse position to the center of the hidden message
+    const centerX = 0; // Adjust the X coordinate as needed
+    const centerY = 0; // Adjust the Y coordinate as needed
+    const radius = 0.5; // Adjust the radius size as needed
+
+    const dx = x - rect.width / 2;
+    const dy = y - rect.height / 2;
+    const distanceToCenter = Math.sqrt(dx * dx + dy * dy);
+
+    // Adjust the opacity of the hidden message material based on the distance from the mouse to the center
+    if (distanceToCenter < radius * rect.width / 2) {
+        hiddenMessageMaterial.opacity = 1;
+    } else {
+        hiddenMessageMaterial.opacity = 0;
     }
+});
 
-    document.getElementById('hiddenMessageContainer').addEventListener('mousemove', (event) => {
-        const container = document.getElementById('hiddenMessageContainer');
-        const rect = container.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
+init();
 
-        const normalizedX = (x / rect.width) * 2 - 1;
-        const normalizedY = -(y / rect.height) * 2 + 1;
-
-        const mouseVector = new THREE.Vector3(normalizedX, normalizedY, 0.5);
-        mouseVector.unproject(camera);
-
-        const dir = mouseVector.sub(camera.position).normalize();
-        const distance = -camera.position.z / dir.z;
-        const pos = camera.position.clone().add(dir.multiplyScalar(distance));
-
-        flashlight.target.position.copy(pos);
-    });
-
-    initFlashlight();
 };
 
